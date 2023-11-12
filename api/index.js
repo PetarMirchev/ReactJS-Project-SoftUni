3814414563ac8e8b6494df1c084435191128b11b
Petar Mirchev <petar.g.mirchev@gmail.com>
2023-11-12 10:35:17 +0200

    cors fix

--- api/index.js
+++ api/index.js
@@ -6,6 +6,21 @@ dotenv.config();
 
 import mongoose from "mongoose";
 
+//*************************************************************************** */
+import cors from "cors";
+
+// const cors = () => (req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Auth');
+//     res.setHeader('access-control-expose-headers', 'Set-Cookie');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     next();
+// };
+
+
+
+
 //************************************************************************************* */
 
 //import on file 'routes'
@@ -16,40 +31,6 @@ import usersRoute from "./routes/users.js";
 //********************************************************************************** */
 
 import cookieParser from "cookie-parser";
-
-//*************************************************************************** */
-
-import cors from "cors";
-// const cors = require('cors');
-
-//"cors" for fix Access to XMLHttpRequest at '...' from origin 'localhost:3000' has been blocked by CORS policy in development mode 
-//This fix the POST request from react app in development mode. 
-app.use(cors({
-    // origin: 'http://localhost:3000',
-    // origin: '*',
-    origin: 'http://127.0.0.1:3000/',
-    methods: ['POST', 'PUT', 'GET', 'PATCH', 'DELETE'],
-    header: 'Content-Type, application/json',
-
-}));
-
-// (alias) function cors<T extends e.CorsRequest = e.CorsRequest>(options?: e.CorsOptions | e.CorsOptionsDelegate<T>): (req: T, res: {
-//     statusCode?: number | undefined;
-//     setHeader(key: string, value: string): any;
-//     end(): any;
-// }, next: (err?: any) => any) => void
-
-
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.set('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
-//     res.header('Access-Control-Allow-Credentials', true);
-//     res.header('Access-Control-Allow-Origin', req.headers.origin);
-  
-//     next();
-//   });
 
 
 
@@ -89,6 +70,17 @@ app.listen(8800, () => {
 //***************************************************************************************************************** */
 
 
+
+//"cors" for fix Access to XMLHttpRequest at '...' from origin 'localhost:3000' has been blocked by CORS policy in development mode 
+//This fix the POST request from react app in development mode. 
+app.use(cors());
+
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     methods: "GET,POST,PUT,DELETE",
+//     credentials: true,
+//   }));
+// app.options('*', cors())
 
 //middleware
 app.use(cookieParser());
