2ee4eef7e7d8273d73607d76fe2d4c7591e5a4c0
Petar Mirchev <petar.g.mirchev@gmail.com>
2023-11-18 16:23:19 +0200

    Update AboutContent.jsx

--- client/src/AboutPage/AboutContent/AboutContent.jsx
+++ client/src/AboutPage/AboutContent/AboutContent.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import './bannert.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const AboutContent = () => {
   return (
     <div>
 
-<div class="our-services">
-		<div class="about-us">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-6">
-						<div class="left-image">
+<div className="our-services">
+		<div className="about-us">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-6">
+						<div className="left-image">
 							<img src="images/about-left-image.jpg" alt=""/>
 						</div>
 					</div>
-					<div class="col-lg-6">
-						<div class="right-content">
+					<div className="col-lg-6">
+						<div className="right-content">
 							<h4>About Us &amp; Our Skills</h4>
 							<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod kon tempor incididunt ut labore.</span>
-							<div class="quote">
-								<i class="fa fa-quote-left"></i><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiuski smod kon tempor incididunt ut labore.</p>
+							<div className="quote">
+								<i className="fa fa-quote-left"></i><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiuski smod kon tempor incididunt ut labore.</p>
 							</div>
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod kon tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
 							<ul>
-								<li><a href='/'><i class="fa-brands fa-square-facebook"></i></a></li>
-								<li><a href='/'><i class="fa fa-linkedin"></i></a></li>
+								<li><a href='/'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
+                                            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                                        </svg>
+                                    </a>
+                                </li>
+								<li><a href='/'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
+                                            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                                        </svg>
+                                    </a>
+                                </li>
 							</ul>
 						</div>
 					</div>
@@ -35,31 +45,31 @@ const AboutContent = () => {
         </div>
 
 
-    <div class="our-services">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="section-heading">
+    <div className="our-services">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="section-heading">
                         <h2>Our Services</h2>
                         <span>Details to details is what makes Hexashop different from the other themes.</span>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="service-item">
+                <div className="col-lg-4">
+                    <div className="service-item">
                         <h4>Synther Vaporware</h4>
                         <p>Lorem ipsum dolor sit amet, consecteturti adipiscing elit, sed do eiusmod temp incididunt ut labore, et dolore quis ipsum suspend.</p>
                         <img src="images/service-01.jpg" alt="service3"/>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="service-item">
+                <div className="col-lg-4">
+                    <div className="service-item">
                         <h4>Locavore Squidward</h4>
                         <p>Lorem ipsum dolor sit amet, consecteturti adipiscing elit, sed do eiusmod temp incididunt ut labore, et dolore quis ipsum suspend.</p>
                         <img src="images/service-02.jpg" alt="service2"/>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="service-item">
+                <div className="col-lg-4">
+                    <div className="service-item">
                         <h4>Health Gothfam</h4>
                         <p>Lorem ipsum dolor sit amet, consecteturti adipiscing elit, sed do eiusmod temp incididunt ut labore, et dolore quis ipsum suspend.</p>
                         <img src="images/service-03.jpg" alt="service1"/>
